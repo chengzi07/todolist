7d984760c7524ae4d7a2dbeba0ec8d82f04ff385
chengzi07 <yihanchen7@163.com>
2019-03-27 16:38:31 +0800

    仿照注册，完成登录功能

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
-import {signUp} from './leanCloud'
+import {signUp,signIn} from './leanCloud'
 export default class UserDialog extends Component{
   constructor(props){
     super(props)
@@ -19,16 +19,26 @@ export default class UserDialog extends Component{
   }
   signUp(e){
     e.preventDefault()
-    let {username,password}=this.state.formData
-    let success=(user)=>{
-      this.props.onSignUp.call(null,user)
+    let {username, password} = this.state.formData
+    let success = (user)=>{
+      this.props.onSignUp.call(null, user)
     }
-    let error=(error)=>{
-      console.log(error)
+    let error = (error)=>{
+      alert(error)
     }
-    signUp(username,password,success,error)
+    signUp(username, password, success, error)
   }
-  signIn(e){}
+  signIn(e){
+    e.preventDefault()
+    let {username, password} = this.state.formData
+    let success = (user)=>{
+      this.props.onSignIn.call(null, user)
+    }
+    let error = (error)=>{
+      alert(error)
+    }
+    signIn(username, password, success, error)
+  }
   changeFormData(key,e){
     let stateCopy=JSON.parse(JSON.stringify(this.state)) // 用JSON深拷贝
     stateCopy.formData[key]=e.target.value
